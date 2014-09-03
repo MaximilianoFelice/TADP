e4ec3d48efb92cbcc2cee4184e4caa73411456cb
MaximilianoFelice <maximilianofelice@gmail.com>
2014-09-03 13:34:29 -0300

    Initial Commit: Project creation

---
+++ TP1-Metaprogramming/lib/TP1/Metaprogramming.rb
@@ -0,0 +1,7 @@
+require "TP1/Metaprogramming/version"
+
+module TP1
+  module Metaprogramming
+    # Your code goes here...
+  end
+end
---
+++ TP1-Metaprogramming/lib/TP1/Metaprogramming/version.rb
@@ -0,0 +1,5 @@
+module TP1
+  module Metaprogramming
+    VERSION = "0.0.1"
+  end
+end
