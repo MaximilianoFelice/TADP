7ce78a183e15f7dd3fabe4afddea7d8ab610ddbc
MaximilianoFelice <maximilianofelice@gmail.com>
2014-09-10 20:18:15 -0300

    Singleton Module Behaviour Refactor: Duplicated code.

--- TP1-Metaprogramming/lib/TP1/Singleton_Module_Behaviour.rb
+++ TP1-Metaprogramming/lib/TP1/Singleton_Module_Behaviour.rb
@@ -1,27 +1,8 @@
 module TP
 
-  module Singleton_Module
+  module Singleton_Module_Accessors
 
     attr_accessor :singleton_module
-
-    # TODO: Buscar una forma de no repetir codigo
-    # If the module is included by a class, it defines behaviour for every new instance
-    def self.included(including_class)
-      including_class.singleton_class.class_eval{attr_accessor :singleton_module
-
-                                                  def singleton_module
-
-                                                    if !@singleton_module then
-                                                      @singleton_module = Module.new
-                                                      self.extend @singleton_module
-                                                    end
-
-                                                    @singleton_module
-
-                                                  end
-
-                                                  }
-    end
 
     def singleton_module
 
@@ -32,6 +13,18 @@ module TP
 
       @singleton_module
 
+    end
+
+  end
+
+  module Singleton_Module
+
+    include Singleton_Module_Accessors
+
+    # TODO: Buscar una forma de no repetir codigo
+    # If the module is included by a class, it defines behaviour for every new instance
+    def self.included(including_class)
+      including_class.singleton_class.include TP::Singleton_Module_Accessors
     end
 
   end
