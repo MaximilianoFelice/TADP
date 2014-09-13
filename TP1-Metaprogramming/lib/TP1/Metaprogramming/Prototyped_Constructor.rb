2a721ac631db820f7e0b18e09d8b055c82bb885b
MaximilianoFelice <maximilianofelice@gmail.com>
2014-09-13 16:40:56 -0300

    Added copy instance behaviour, with its corresponding tests

--- TP1-Metaprogramming/lib/TP1/Metaprogramming/Prototyped_Constructor.rb
+++ TP1-Metaprogramming/lib/TP1/Metaprogramming/Prototyped_Constructor.rb
@@ -7,6 +7,14 @@ module TP
 
     attr_accessor :constructor, :prototype
 
+    def self.included(base)
+    base.instance_eval{
+      def self.copy(an_instance)
+        self.new(an_instance)
+      end
+    }
+    end
+
     def initialize(instance, block = proc{})
       @prototype = instance
       @constructor = block
@@ -20,6 +28,8 @@ module TP
 
       if (args[0].class == Hash) then
         self.build_with_hash(new_object, args[0])
+      elsif (args.length == 0)
+        self.copy_instance
       else
         build_with_block(new_object, *args)
       end
@@ -33,6 +43,10 @@ module TP
     def build_with_hash(new_object, hash)
       hash.each{ |key, value| new_object.instance_module_variable_set(key, value) }
       return new_object
+    end
+
+    def copy_instance
+      @prototype.clone
     end
   end
 
--- TP1-Metaprogramming/test/Prototyped_Constructor_Tests/[42]Prototyped_Constructors_spec.rb
+++ TP1-Metaprogramming/test/Prototyped_Constructor_Tests/[42]Prototyped_Constructors_spec.rb
@@ -1,3 +1,4 @@
+
 require 'rspec'
 require "TP1/Metaprogramming"
 
@@ -6,8 +7,8 @@ describe 'Let Constructors build Prototypes' do
   before(:all) do
     @guerrero = TP::PrototypedObject.new
     @guerrero.set_property(:energia, 0)
-    @guerrero.set_property(:potencial_defensivo, 0)
-    @guerrero.set_property(:potencial_ofensivo, 0)
+    @guerrero.set_property(:potencial_defensivo, 10)
+    @guerrero.set_property(:potencial_ofensivo, 10)
   end
 
   it 'should set a new constructor based on a prototype' do
@@ -29,6 +30,29 @@ describe 'Let Constructors build Prototypes' do
     Guerrero = TP::ProtoConstructor.new(@guerrero)
     un_guerrero = Guerrero.new({energia: 100, potencial_ofensivo: 30, potencial_defensivo: 10})
     expect(un_guerrero.potencial_ofensivo).to eq(30)
+
+  end
+
+  it 'should copy prototype state' do
+
+    Guerrero = TP::ProtoConstructor.copy(@guerrero)
+    un_guerrero = Guerrero.new
+    expect(un_guerrero.potencial_defensivo).to eq(10)
+
+  end
+
+  it 'should keep different contexts for every copy' do
+
+    Guerrero = TP::ProtoConstructor.copy(@guerrero)
+    un_guerrero = Guerrero.new
+    otro_guerrero = Guerrero.new
+
+    otro_guerrero.energia = 1500
+    otro_guerrero.potencial_defensivo = 102
+    un_guerrero.potencial_defensivo = 40
+    expect(un_guerrero.potencial_defensivo).to eq(40)
+    expect(otro_guerrero.potencial_defensivo).to eq(102)
+    expect(otro_guerrero.energia).to eq(1500)
 
   end
 
