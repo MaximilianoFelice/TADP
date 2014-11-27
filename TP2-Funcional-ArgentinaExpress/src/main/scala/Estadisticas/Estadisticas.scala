e613f6d0fd185859be01cb7152af097917d93cc2
MaximilianoFelice <maximilianofelice@gmail.com>
2014-11-27 17:00:58 -0300

    Changes

--- TP2-Funcional-ArgentinaExpress/src/main/scala/Estadisticas/Estadisticas.scala
+++ TP2-Funcional-ArgentinaExpress/src/main/scala/Estadisticas/Estadisticas.scala
@@ -13,42 +13,75 @@ import Viaje.Viaje
 package object EstadisticasPack {
 
   case object Estadisticas {
-    var viajes: Set[Viaje] = Set()
-    var transportes: Set[Transporte] = Set()
-    var envios: Set[Envio] = Set()
+    var viajes: List[Viaje] = List()
+    var transportes: List[Transporte] = List()
+    var envios: List[Envio] = List()
 
     def sucursales = viajes.sucursales
-    def addViaje (viaje: Viaje) = viajes += viaje
+    def addViaje (viaje: Viaje) = viajes :+ viaje
+
+    def Get[T](tipo: T): Set[T]={
+        ???
+    }
   }
 
-  implicit class Viajes (viajes: Set[Viaje]){
-    def sucursales: Set[Sucursal] = viajes.flatMap(v => v.sucursales)
-    def costoPromedio: Double = viajes.promedio(_.costo)
-    def tiempoPromedio: Double = viajes.promedio(_.tiempo)
-    def gananciaBrutaPromedio: Double = viajes.promedio(_.gananciaBruta)
-    def cantidadEnvios: Int = viajes.map(_.cantidadEnvios).sum
-    def cantidadViajes: Int = viajes.size
-    def facturacionTotal: Double = viajes.map(_.beneficio).sum
-
-    def transportes: Set[Transporte] = viajes.map(_.transporte)
-    def envios: Set[Envio] = viajes.flatMap(_.envios)
-
-    def filtroPorCaracteristica(carac: Caracteristica): Set[Viaje] = viajes.filter(v => v.transporte.caracteristicas.contains(carac))
-
-    def promedio(criterio: Viaje => Double): Double = viajes.map(criterio).sum / viajes.size
+  trait filterable[+A] {
+    def col : List[A]
+    def Where(filt: A => Boolean): List[A] = col.filter(filt)
   }
 
-  implicit class Transportes[+T <: Transporte] (transportes: Seq[T]){
-    def envios: Set[Envio] = Estadisticas.envios
-    def filtroPorCaracteristica(carac: Caracteristica): Seq[T] = transportes.filter(t => t.caracteristicas.contains(carac))
-    def filtroPorTipo[G <: Transporte](tipo: G): Seq[T] = transportes.filter(t => t match {
+  implicit class operatorsExt[T <: Double](left: T){
+    /* TODO: Abstraer logica repetida */
+    def gT[A <: Double, B <: Double](l: A)(r: B): Boolean = (l > r)
+    def lT[A <: Double, B <: Double](l: A)(r: B): Boolean = (l < r)
+    def equa[A <: Double, B <: Double](l: A)(r: B): Boolean = (l == r)
+    def greaterThan[A, F <: Double](right: A => F) = gT(left)_ compose right
+    def lesserThan[A, F <: Double](right: A => F): A => Boolean = lT(left)_ compose right
+    def equalsThan[A, F <: Double](right: A => F): A => Boolean = equa(left)_ compose right
+
+  }
+  implicit class binaryExt[A](left: A => Boolean){
+    def partialForAll[A](coll: Set[A => Boolean])(elem: A): Boolean = coll.forall(x => x(elem))
+    def partialAny[A](coll: Set[A => Boolean])(elem: A): Boolean = coll.exists(x => x(elem))
+    /* TODO: Abstraer logica repetida */
+    def and(right: A => Boolean): A => Boolean = partialForAll(Set(left, right))
+    def or(right: A => Boolean): A => Boolean = partialAny(Set(left, right))
+  }
+
+
+
+  implicit class Viajes (val col: List[Viaje]) extends filterable[Viaje]{
+    def sucursales: List[Sucursal] = col.flatMap(v => v.sucursales)
+    def costoPromedio: Double = col.promedio(_.costo)
+    def tiempoPromedio: Double = col.promedio(_.tiempo)
+    def gananciaBrutaPromedio: Double = col.promedio(_.gananciaBruta)
+    def cantidadEnvios: Int = col.map(_.cantidadEnvios).sum
+    def cantidadViajes: Int = col.size
+    def facturacionTotal: Double = col.map(_.beneficio).sum
+
+    def transportes: List[Transporte] = col.map(_.transporte)
+    def envios: List[Envio] = col.flatMap(_.envios)
+
+    def filtroPorCaracteristica(carac: Caracteristica): List[Viaje] = col.filter(v => v.transporte.caracteristicas.contains(carac))
+
+    def promedio(criterio: Viaje => Double): Double = col.map(criterio).sum / col.size
+  }
+
+  implicit class Transportes[+T <: Transporte] (val col: List[T]) extends filterable[T]{
+    def envios: List[Envio] = Estadisticas.envios
+    def filtroPorCaracteristica(carac: Caracteristica): List[T] = col.filter(t => t.caracteristicas.contains(carac)).toList
+    def filtroPorTipo[G <: Transporte](tipo: G): List[T] = col.filter(t => t match {
       case a if a.getClass == `tipo`.getClass => true
       case _ => false})
   }
 
-  implicit class Envios (envios: Set[Envio]){
-    def filtroPorCaracteristica(carac: Caracteristica): Envios = envios.filter(_.caracteristicas.contains(carac))
+  implicit class Envios (val col: List[Envio]) extends filterable[Envio]{
+    def filtroPorCaracteristica(carac: Caracteristica): Envios = col.filter(_.caracteristicas.contains(carac))
   }
 
+  implicit class Sucursales (val col: List[Sucursal]) extends filterable[Sucursal]{
+    val facturacionTotal: Sucursal => Double = {_ => 1000}
+    val aSet: List[Sucursal] = List(new Sucursal(10), new Sucursal(100))
+  }
 
 }
--- TP2-Funcional-ArgentinaExpress/src/test/scala/Estadisticas/Control_Estadisticas.scala
+++ TP2-Funcional-ArgentinaExpress/src/test/scala/Estadisticas/Control_Estadisticas.scala
@@ -2,10 +2,10 @@ package ArgentinaExpress.tests
 
 import ArgentinaExpress.Caracteristica.Caracteristicas._
 import ArgentinaExpress.Envio.Envios._
+import ArgentinaExpress.Estadisticas.EstadisticasPack._
 import ArgentinaExpress.Sucursal.Sucursal
 import ArgentinaExpress.Transporte.Transportes._
 import ArgentinaExpress.Transporte._
-import ArgentinaExpress.Estadisticas.EstadisticasPack._
 import org.scalatest.{BeforeAndAfter, FunSuite}
 
 /**
@@ -17,12 +17,12 @@ class Control_Estadisticas extends FunSuite with BeforeAndAfter {
   var sucursal2: Sucursal  = _
   var sucursal3: Sucursal  = _
   var sucursal4: Sucursal = _
-  var sucursales: Set[Sucursal] = _
+  var sucursales: List[Sucursal] = _
   var env1: Envio = _
   var env2: Envio = _
   var env3: Envio = _
   var env4: Envio = _
-  var envios: Set[Envio] = _
+  var envios: List[Envio] = _
   var camion: Camion = _
   var f100: Furgoneta = _
   var avion: Avion = _
@@ -35,27 +35,28 @@ class Control_Estadisticas extends FunSuite with BeforeAndAfter {
     sucursal2 = new Sucursal(8)
     sucursal3 = new Sucursal(7)
     sucursal4 = new Sucursal(20)
-    sucursales = Set(sucursal1, sucursal2, sucursal3, sucursal4)
+    sucursales = List(sucursal1, sucursal2, sucursal3, sucursal4)
     env1 = Envio(10, sucursal1, 5, Seq(Refrigerado))
     env2 = Envio(15, sucursal2, 2, Seq(Urgente, Refrigerado))
     env3 = Envio(8, sucursal1, 3, Seq(ConAnimales))
     env4 = Envio(14, sucursal3, 4, Seq(Normal, SustanciasPeligrosas))
-    envios = Set(env1, env2, env3)
+    envios = List(env1, env2, env3)
     camion = Camion(Seq(Normal, SustanciasPeligrosas))
     f100 = Furgoneta(Seq(Refrigerado, ConAnimales))
     avion = Avion(Seq(Refrigerado))
     camionBis = Camion(Seq(Urgente, Refrigerado))
-    transportes = Seq(camion, camionBis, f100)
+    transportes = List(camion, camionBis, f100)
+    //transportes = Seq(camion, camionBis)
 
     camion.agregarEnvio(env4)
     f100.agregarEnvio(env3)
     avion.agregarEnvio(env1)
     camionBis.agregarEnvio(env2)
 
-    camiones = Seq(camion, camionBis)
+    //camiones = Seq(camion, camionBis)
 
     Estadisticas.envios = envios
-    //Estadisticas.transportes = transportes
+    Estadisticas.transportes = List(camion, camionBis, f100)
   }
 
   test("Tiene sentido") {
@@ -65,6 +66,22 @@ class Control_Estadisticas extends FunSuite with BeforeAndAfter {
   test("Filtro sucursales por camiones"){
     assert(transportes.filtroPorTipo(Camion()) == Seq(camion, camionBis))
       //filtroPorCaracteristica(Urgente)
+  }
+
+  test("Obtener listas del modulo"){
+
+    val facturacionTotal: Sucursal => Double = {_ => 1000}
+
+    Estadisticas.sucursales Where ((1000.0 greaterThan facturacionTotal) or (100.0 ))
+
+    Estadisticas.sucursales Where (1000 < facturacionTotal(_))
+
+
+    def algo(x: Int)(y: Int): Double = x + y
+
+    Estadisticas.sucursales Where {x => true}
+
+    def test = 100.0 greaterThan algo(3)
   }
 
 }
